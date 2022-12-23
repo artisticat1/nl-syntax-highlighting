e44a15467886d6d159193629bb146338c89d2df6
artisticat <100622574+artisticat1@users.noreply.github.com>
2022-12-23 23:26:05 +0000

    feat: highlight adjectives, adverbs, verbs, conjunctions

--- syntaxHighlight.ts
+++ syntaxHighlight.ts
@@ -1,5 +1,6 @@
 import { EditorView, ViewUpdate, Decoration, DecorationSet, ViewPlugin } from "@codemirror/view";
-import nlp from 'compromise';
+import nlp from "compromise";
+import CompromiseView from "compromise/types/view/one";
 
 
 function getDecos(view: EditorView) {
@@ -10,20 +11,42 @@ function getDecos(view: EditorView) {
 		const docText = view.state.sliceDoc(visibleRange.from, visibleRange.to);
 
 		const doc = nlp(docText);
+        const wordsToHighlight: { [partOfSpeech: string]: CompromiseView }  = {};
+
+        const adjectives = doc.adjectives();
 		const nouns = doc.match("#Noun").not("#Pronoun").not("#Possessive");
-		console.log(nouns.out("offset"));
+        const adverbs = doc.adverbs();
+        const verbs = doc.match("#Verb");
+        const conjunctions = doc.conjunctions();
 
-		for (const noun of nouns.out("offset")) {
-			const offset = noun.offset;
-			const start = offset.start + visibleRange.from;
-			const end = start + offset.length;
+        wordsToHighlight["adjective"] = adjectives;
+        wordsToHighlight["noun"] = nouns;
+        wordsToHighlight["adverb"] = adverbs;
+        wordsToHighlight["verb"] = verbs;
+        wordsToHighlight["conjunction"] = conjunctions;
 
-			widgets.push(Decoration.mark({
-				inclusive: true,
-				attributes: {},
-				class: "noun"
-			}).range(start, end));
-		}
+
+        for (const partOfSpeech of Object.keys(wordsToHighlight)) {
+            const words = wordsToHighlight[partOfSpeech];
+            // console.log(words);
+
+            // Use .out("offset") to get the index of the word's start and end in the document
+            for (const word of words.out("offset")) {
+
+                const offset = word.offset;
+                if (offset.length === 0) continue;
+                const start = offset.start + visibleRange.from;
+
+                // Use .terms[0].offset to ignore punctuation
+                const end = start + word.terms[0].offset.length;
+
+                widgets.push(Decoration.mark({
+                    inclusive: true,
+                    attributes: {},
+                    class: partOfSpeech
+                }).range(start, end));
+            }
+        }
 	}
 
 	return Decoration.set(widgets, true);
@@ -38,7 +61,7 @@ export const NLSyntaxHighlightViewPlugin = ViewPlugin.fromClass(class {
     }
 
     update(update: ViewUpdate) {
-		if (update.docChanged) {
+		if (update.docChanged || update.viewportChanged) {
 			this.decorations = getDecos(update.view);
 		}
     }
